ee678ae2c252ffd35d8822295109a13177d27694
Grimmer <grimmer0125@gmail.com>
2022-08-01 22:41:42 +0800

    docs: add some Pydantic usage notes

--- lawtechhackson/main.py
+++ lawtechhackson/main.py
@@ -16,6 +16,14 @@ def read_file(dataset_folder, court_folder, file_name:str):
     with open(path) as f:
         json_data = json.load(f)
         resp = Judgment(**json_data)
+
+    # some usage notes about pydantic 
+    # 1. 如果是用 Judgment(**json_data)，它預設會檢查型斜
+    #   1. 如果某些 fields 跟指定的 type 不一樣，它會把某些 type 自動轉型，像 int -> str. 但如果是塞 []　到 str type,
+        # 它會 throw error 
+    # 2. 如果是已產生的 Pydantic instance，然後再 assign field，預設不會檢查 type，所以可以塞 [] 到 str type field，    
+    #    1. 但可以 validate_assignment=True 來 enable 檢查型別
+  
     return resp 
 
 def analysis(judgment: Judgment):
@@ -31,7 +39,9 @@ def main():
     # NOTE: first test file 
     # TODO: load a batch of files
     file_name = "民事裁定_110,簡聲抗,19_2021-09-29.json"
+    # judgment = read_file("", "sample_data", file_name)
     judgment = read_file(settings.LAWSNOTE_JUDGMENT_PATH, f"{Court.Taiwan_Shilin_District_Court}_{Law.Civil}", file_name)
+
     result = analysis(judgment)
     load_to_db(result)
     
