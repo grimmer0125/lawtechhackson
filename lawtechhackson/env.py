75d914bfc2be9001d4ad861f013be503943ffd01
Grimmer <grimmer0125@gmail.com>
2022-08-03 22:26:34 +0800

    feat: add missing DroidTownSettings class

--- lawtechhackson/env.py
+++ lawtechhackson/env.py
@@ -1,9 +1,16 @@
 from pydantic import BaseSettings
 
 
+class DroidTownSettings(BaseSettings):
+    droid_username = ""
+    droid_api_key = ""
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
 class DataBaseSettings(BaseSettings):
-    mongo_user = ""
-    mongo_pwd = ""
     mongo_connect_str = ""
 
     class Config:
