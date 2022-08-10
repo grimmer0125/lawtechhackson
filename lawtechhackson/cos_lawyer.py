e0a98433ff74b46bec6147c22605e7021ae1dfbf
Grimmer <grimmer0125@gmail.com>
2022-08-11 00:22:37 +0800

    style: use yapf to format cos_lawyer.py

--- lawtechhackson/cos_lawyer.py
+++ lawtechhackson/cos_lawyer.py
@@ -6,28 +6,30 @@ from sklearn.metrics.pairwise import cosine_similarity
 from typing import List
 
 
-def get_similiar_lawyers( cosine_sim:np.ndarray, input_lawyer:int = 3) -> List[int]:
-    # Get the similarity scores                                                               
+def get_similiar_lawyers(cosine_sim: np.ndarray,
+                         input_lawyer: int = 3) -> List[int]:
+    # Get the similarity scores
     sim_scores = list(enumerate(cosine_sim))
 
-    # Sort the layers based on the similarity scores                                          
+    # Sort the layers based on the similarity scores
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-    # Get the scores of the top-k most similar articles                                       
+    # Get the scores of the top-k most similar articles
     sim_scores = sim_scores[1:input_lawyer]
 
-    # Get the laywer indices                                                                  
+    # Get the laywer indices
     lawyer_indices = [i[0] for i in sim_scores]
 
-    # Return the top-k most similar article ID                                                
+    # Return the top-k most similar article ID
     return lawyer_indices
-
 
 
 def lawyer_query(user_input: List, model, lawyer_emd) -> List[str]:
     query_emd = model.encode(user_input)
     search_emd = np.array(list(lawyer_emd.values()))
-    cosine_sim = cosine_similarity(search_emd, query_emd) # using cosine_sim in scikit learn for quick access
+    cosine_sim = cosine_similarity(
+        search_emd,
+        query_emd)  # using cosine_sim in scikit learn for quick access
     lawer_indces = get_similiar_lawyers(cosine_sim, 3)
     lawyer = np.array(list(lawyer_emd.keys()))
 
