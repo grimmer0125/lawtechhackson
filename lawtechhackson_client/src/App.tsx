9c3edae605c88d13f794dbf5f1479049f28d85a0
Grimmer <grimmer0125@gmail.com>
2022-08-13 18:00:38 +0800

    feat(client): add material-ui package

--- lawtechhackson_client/src/App.tsx
+++ lawtechhackson_client/src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  height: "100vh"
+}));
+
+/** emotion: https://mui.com/zh/material-ui/guides/interoperability/#emotion */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid xs={4}>
+            <Item>Search Panel</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Lawyers Results Panel</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Lawyer Detail Panel</Item>
+          </Grid>
+        </Grid>
+      </Box>
+      {/* https://mui.com/material-ui/react-container/ */}
+      {/* <CssBaseline /> */}
+      {/* <Container maxWidth="sm"> */}
+      {/* <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} >
+        <div>
+          do something
+        </div>
+      </Box>
+      {/* </Container> */}
+    </React.Fragment>
   );
 }
 
