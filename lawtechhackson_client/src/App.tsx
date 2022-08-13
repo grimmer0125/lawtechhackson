f09a9e2ac22e9ed609572c6a854ce7e97ac8b762
Grimmer <grimmer0125@gmail.com>
2022-08-13 19:13:15 +0800

    feat(client): finish Query Panel and Lawyers Results Panel

--- lawtechhackson_client/src/App.tsx
+++ lawtechhackson_client/src/App.tsx
@@ -3,6 +3,20 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import axios from 'axios';
+
+interface LawyerProfile {
+  name: string;
+  now_lic_no: string;
+  guilds: string[];
+  office: string;
+  total_litigates: number;
+  win_rate: number;
+  law_issues: string[];
+}
+
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,17 +28,94 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 /** emotion: https://mui.com/zh/material-ui/guides/interoperability/#emotion */
+// https://emotion.sh/docs/introduction
+/** its css https://mui.com/zh/material-ui/guides/interoperability/#the-css-prop */
 
 function App() {
+
+  const [searchValue, setSearchValue] = React.useState('');
+  const [lawyerProfileList, setLawyerProfileList] = React.useState<LawyerProfile[]>([]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+
+  const onClick = async (evt?: any) => {
+    console.log("click event:", evt)
+
+    if (!searchValue) {
+      return;
+    }
+
+
+    const api = "http://localhost:8000/query-lawyer"
+    const resp = await axios.post(api, {
+      question: searchValue,
+    })
+    const { data } = resp;
+    console.log({ data });
+    if (Array.isArray(data)) {
+      // todo: update ui
+      setLawyerProfileList(data);
+    }
+  }
+
   return (
     <React.Fragment>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={1}>
           <Grid xs={4}>
-            <Item>Search Panel</Item>
+            <Item>Prefect Match
+              <Box
+                component="form"
+                sx={{
+                  '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                autoComplete="off"
+              >
+                <TextField style={{ width: 350 }} value={searchValue} onChange={handleChange}
+                  onKeyPress={(event) => {
+                    if (event.key === 'Enter') {
+                      console.log('Enter Pressed')
+                      event.preventDefault();
+                      onClick();
+                    }
+                  }}
+                  id="outlined-basic" label="請輸入你的法律問題或關鍵字 (e.g. 股票)" variant="outlined" />
+              </Box>
+              <Button variant="contained" onClick={onClick}>送出</Button>
+            </Item>
           </Grid>
           <Grid xs={4}>
-            <Item>Lawyers Results Panel</Item>
+            <Item>
+              <div>
+
+                {/* <Paper style={{ height: "100vh" }}> */}
+                Lawyers Results Panel
+                {lawyerProfileList.map((lawyerProfile) => {
+                  const { name, now_lic_no, guilds, total_litigates, win_rate, law_issues } = lawyerProfile;
+                  return (
+                    <Box key={now_lic_no}>
+                      <Button >
+                        {`${name}, ${now_lic_no}, ${guilds}, 官司數:${total_litigates}, 勝率:${win_rate} , ${law_issues}`}
+
+                        {/* name: string;
+                            now_lic_no: string;
+                            guilds: string[];
+                            office: string;
+                            total_litigates: number;
+                            win_rate: number;
+                            law_issues: string[]; 
+                      */}
+
+                      </Button>
+                    </Box>);
+
+                })}
+                {/* </Paper> */}
+              </div>
+            </Item>
           </Grid>
           <Grid xs={4}>
             <Item>Lawyer Detail Panel</Item>
@@ -40,7 +131,7 @@ function App() {
         </div>
       </Box>
       {/* </Container> */}
-    </React.Fragment>
+    </React.Fragment >
   );
 }
 
